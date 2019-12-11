978bc0db52c6dfd3ed504fd7c6d88fab1a816cb7
Montenegro <lauflogik@gmail.com>
2019-12-10 23:36:59 -0600

    Added GeneralTree

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -11,6 +11,11 @@ namespace StringCollections
     {
         public static void Main(string[] args)
         {
+            UsingTree();
+            //UsingList();
+        }
+        public static void UsingList()
+        {
 
             TimeSpan sortTime;
             TimeSpan ZTime;
@@ -59,7 +64,75 @@ namespace StringCollections
 
             Console.ReadLine();
         }
+        public static void UsingTree()
+        {
 
+            TimeSpan sortTime;
+            TimeSpan ZTime;
+            TimeSpan HeTime;
+            TimeSpan eTime;
+            DateTime start;
+
+            List<string> words = ReadInWords("words.txt");
+            Tree<string> tree = new Tree<string>();
+            
+            for (char i = 'a'; i < 'z'; i++)
+            {
+                tree.Root.AddChild(i.ToString());
+            }
+
+            
+            start = DateTime.Now;
+            words.Sort((a, b) => b.CompareTo(a));
+            sortTime = DateTime.Now - start;
+
+            foreach (var word in words)
+            {
+                if (word[0] >= 'a' && word[0] <= 'z')
+                {
+                    tree.Root.Nodes[word[0] - 'a'].AddChild(word);
+                }
+                else
+                {
+                    tree.Root.AddChild(word);
+                }
+            }
+
+            ///PrintWords(words, "Full List");
+
+            start = DateTime.Now;
+            List<string> wordsBeginWithZ = tree.Where(x => x.ToLower().StartsWith("z")).ToList();
+            ZTime = DateTime.Now - start;
+
+            /*
+            Console.WriteLine("   {0,-35} {1,20}", "Value of Minutes Component:", interval.Minutes);
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Minutes:", interval.TotalMinutes);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Seconds Component:", interval.Seconds);
+            */
+            PrintWords(wordsBeginWithZ, "Begins with Z");
+
+
+
+            start = DateTime.Now;
+            List<string> wordsBeginWithHe = tree.Where(x => x.ToLower().StartsWith("he")).ToList();
+            HeTime = DateTime.Now - start;
+
+            PrintWords(wordsBeginWithHe, "Begins with He");
+
+
+            start = DateTime.Now;
+            List<string> wordsWithEAtSecondPos = tree.Where(x => x != null && x.Length > 1 && x.ToLower()[1] == 'e').ToList();
+            eTime = DateTime.Now - start;
+            PrintWords(wordsWithEAtSecondPos, "Has e second position");
+
+
+            Console.WriteLine("Total Time Sort: {0,2:N0}:{1,2:N0}", sortTime.Seconds, sortTime.Milliseconds);
+            Console.WriteLine("Total Time Begins with Z: {0,2:N0}:{1,2:N0}", ZTime.Seconds, ZTime.Milliseconds);
+            Console.WriteLine("Total Time He: {0,2:N0}:{1,2:N0}", HeTime.Seconds, HeTime.Milliseconds);
+            Console.WriteLine("Total Time E: {0,2:N0}:{1,2:N0}", eTime.Seconds, eTime.Milliseconds);
+
+            Console.ReadLine();
+        }
         public static void PrintWords(List<string> words, string header)
         {
 
